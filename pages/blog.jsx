331b6ede04bbdb1193d86e57893704470b2e5696
weblayoutz <web@layoutz.com.br>
2017-05-22 22:54:35 -0700

    Added blog page

---
+++ pages/blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import { config } from './../config' 
+
+export default class Blog extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Blog Page</h1>
+      </div>
+    )
+  }
+}
